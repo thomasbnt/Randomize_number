f5a46ebffcf4e5af8d8a156d81bf8354b449e749
Thomas Bnt <thomasbnt@protonmail.com>
2019-08-28 19:44:33 +0200

    crashtest sw

--- sw.js
+++ sw.js
@@ -4,13 +4,15 @@ self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll([
-                `/`,
                 `/index.html`,
                 `/manifest.json`,
                 `/assets/css/app.css`,
-                `/assets/js/init.js`
-            ])
-                .then(() => self.skipWaiting())
+                `/assets/js/init.js`,
+                `/assets/img/manifest/192x192.png`,
+                `/assets/img/manifest/128x128.png`,
+                `/assets/img/manifest/512x512.png`,
+                `/assets/img/favicon.png`
+            ]).then(() => self.skipWaiting())
         })
     )
 })
